3bb882867e832b5f2c0bb2e1339e3efeae531c9d
Samet Oussema <sametoussema@gmail.com>
2017-01-16 23:03:51 +0100

    add 3 operators in Arethmetic, with failer unit test

--- src/main/java/Arithmetic.java
+++ src/main/java/Arithmetic.java
@@ -3,4 +3,15 @@ class Arithmetic {
     Long add(Long a, Long b) {
         return a + b;
     }
+
+    Long subtract(Long a, Long b) {
+        return a + b;
+    }
+
+    Long multiplication(Long a, Long b) {
+        return a + b;
+    }
+    Long divide(Long a, Long b) {
+        return a + b;
+    }
 }
--- src/test/java/ArithmeticTest.java
+++ src/test/java/ArithmeticTest.java
@@ -17,8 +17,24 @@ public class ArithmeticTest {
     }
 
     @Test
-    public void testCalculer() throws Exception {
+    public void testAdd() throws Exception {
         assertEquals(new Long(4), op.add(new Long(1), new Long(3)));
     }
+
+    @Test
+    public void testMultiplication() throws Exception {
+        assertEquals(new Long(6), op.multiplication(new Long(2), new Long(3)));
+    }
+
+    @Test
+    public void testDivide() throws Exception {
+        assertEquals(new Long((long) 1.5), op.divide(new Long(3), new Long(2)));
+    }
+
+    @Test
+    public void testSubtract() throws Exception {
+        assertEquals(new Long(-2), op.subtract(new Long(1), new Long(3)));
+    }
+
 
 }
