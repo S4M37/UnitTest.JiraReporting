533088ae617d0b5898cfebb339776c4212239d65
Samet Oussema <sametoussema@gmail.com>
2017-01-18 22:05:34 +0100

    set all tests failed

--- src/main/java/Arithmetic.java
+++ src/main/java/Arithmetic.java
@@ -1,18 +1,22 @@
 
 class Arithmetic {
     Long add(Long a, Long b) {
-        return a + b;
+        return Long.valueOf(0);
+        //return a + b;
     }
 
     Long subtract(Long a, Long b) {
-        return a - b;
+        return Long.valueOf(0);
+        //return a - b;
     }
 
     Long multiplication(Long a, Long b) {
-        return a * b;
+        return Long.valueOf(0);
+        //return a * b;
     }
 
     Long divide(Long a, Long b) {
-        return a / b;
+        return Long.valueOf(0);
+        //return a / b;
     }
 }
